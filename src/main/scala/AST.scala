db31797bed3a3b42c5e6e95aebce8eccbed13dda
Lucius Meredith <lgreg.meredith@gmail.com>
2020-03-01 12:21:13 -0800

    added simple reduction semantics

--- src/main/scala/AST.scala
+++ src/main/scala/AST.scala
@@ -18,3 +18,21 @@ case class Add (l : RhoCombExp, r : RhoCombExp) extends RhoCombExp
  * Multiplication of two expressions.
  */
 case class Mul (l : RhoCombExp, r : RhoCombExp) extends RhoCombExp
+
+trait RProc
+trait RComb extends RProc
+trait RName
+
+case object RZeroExp                                    extends RhoCombExp with RComb
+case class  RMsgExp ( a : RName, p : RName )            extends RhoCombExp with RComb
+case class  RDupExp ( a : RName, b : RName, c : RName ) extends RhoCombExp with RComb
+case class  RKillExp( a : RName )                       extends RhoCombExp with RComb
+case class  RFwdExp ( a : RName, b : RName )            extends RhoCombExp with RComb
+case class  RBlExp  ( a : RName, b : RName )            extends RhoCombExp with RComb
+case class  RBrExp  ( a : RName, b : RName )            extends RhoCombExp with RComb
+case class  RSeqExp ( a : RName, b : RName, c : RName ) extends RhoCombExp with RComb
+
+case class RStrExp  ( a : RName )                       extends RhoCombExp with RComb
+case class RParExp  ( l : RhoCombExp, r : RhoCombExp )  extends RhoCombExp with RProc
+
+case class RQuotExp( p : RhoCombExp )                   extends RName
--- src/main/scala/Evaluator.scala
+++ src/main/scala/Evaluator.scala
@@ -7,11 +7,57 @@ import org.bitbucket.inkytonik.kiama.attribution.Attribution
  */
 object Evaluator extends Attribution {
 
-    val value : RhoCombExp => Int =
-        attr {
-            case Num (i)    => i
-            case Add (l, r) => value (l) + value (r)
-            case Mul (l, r) => value (l) * value (r)
+  val value : RhoCombExp => Int =
+    attr {
+      case Num (i)    => i
+      case Add (l, r) => value (l) + value (r)
+      case Mul (l, r) => value (l) * value (r)
+      case _          => -1
+    }
+
+  val reduce : RhoCombExp => RhoCombExp =
+    attr {
+      case RZeroExp => RZeroExp
+      case m@RMsgExp( _, _ )    => m
+      case d@RDupExp( _, _, _ ) => d
+      case k@RKillExp( _ )      => k
+      case fw@RFwdExp( _, _ )   => fw
+      case bl@RBlExp( _, _ )    => bl
+      case br@RBrExp( _, _ )    => br
+      case s@RSeqExp( _, _, _ ) => s
+      case u@RStrExp( _ )       => u
+      case dppar@RParExp( RDupExp( a, b, c ), RMsgExp( ma, mp ) ) => {
+        if ( a == ma ) {
+          RParExp( RMsgExp( b, mp ), RMsgExp( c, mp ) )
         }
+        else dppar
+      }
+      case kppar@RParExp( RKillExp( a ), RMsgExp( ma, mp ) ) => {
+        if ( a == ma ) { RZeroExp } else kppar
+      }
+      case fwppar@RParExp( RFwdExp( a, b ), RMsgExp( ma, mp ) ) => {
+        if ( a == ma ) { RMsgExp( b, mp ) } else fwppar
+      }
+      case blppar@RParExp( RBlExp( a, b ), RMsgExp( ma, mp ) ) => {
+        if ( a == ma ) { RFwdExp( mp, b ) } else blppar
+      }
+      case brppar@RParExp( RBrExp( a, b ), RMsgExp( ma, mp ) ) => {
+        if ( a == ma ) { RFwdExp( b, mp ) } else brppar
+      }
+      case sppar@RParExp( RSeqExp( a, b, c ), RMsgExp( ma, mp ) ) => {
+        if ( a == ma ) { RFwdExp( b, c ) } else sppar
+      }
+      case strppar@RParExp( RStrExp( a ), RMsgExp( ma, mp ) ) => {
+        if ( a == ma ) { 
+          mp match { 
+            case RQuotExp( p ) => p
+            case _ => throw new Exception( s"unexpected nominal: ${mp}" )
+          } 
+        } else strppar
+      }
+      case ppar@RParExp( l@RParExp( _, _ ), e ) => {
+        reduce( RParExp( reduce( l ), e ) )
+      }      
+    }
 
 }
